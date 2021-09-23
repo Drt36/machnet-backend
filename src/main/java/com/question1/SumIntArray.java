28c3b43a473e1565b2cdb7675227f70ea859e7b4
Dharma Raj Thanait <dharmarajthanait25@gmail.com>
2021-09-23 14:38:31 +0545

    Question one completed

--- src/main/java/com/question1/SumIntArray.java
+++ src/main/java/com/question1/SumIntArray.java
@@ -1,4 +1,18 @@
 package com.question1;
 
+import java.util.Arrays;
+
 public class SumIntArray {
+
+    public static void main(String[] args) {
+        int size=50;
+        int[] intArray=new int[size];
+
+        for(int i=1;i<=50;i++){
+            intArray[i-1]=i;
+        }
+
+        int sum= Arrays.stream(intArray).reduce(0,(acc,element)->acc+element);
+        System.out.println("Sum of Given Array: "+sum);
+    }
 }

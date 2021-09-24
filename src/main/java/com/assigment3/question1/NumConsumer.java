211c75a10ec4bbbfa46f804758619450f658cfe6
Dharma Raj Thanait <dharmarajthanait25@gmail.com>
2021-09-24 12:53:49 +0545

    Question one using consumer modified.

--- src/main/java/com/assigment3/question1/NumConsumer.java
+++ src/main/java/com/assigment3/question1/NumConsumer.java
@@ -6,6 +6,7 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class NumConsumer {
+
     public static boolean isPrime(int input){
         int flag = 0;
         for (int i = 1; i <=(input/2); i++) {
@@ -32,23 +33,21 @@ public class NumConsumer {
         return flag==1;
     }
 
-    public static void numFilter(List<Integer> list){
-        List<Integer> oddNumList=list.stream().filter(num->isOdd(num)).collect(Collectors.toList());
-        List<Integer> evenNumList=list.stream().filter(num->isEven(num)).collect(Collectors.toList());
-        List<Integer> primeNumList=list.stream().filter(num->isPrime(num)).collect(Collectors.toList());
-
-        System.out.println("-------Filtered Numbers------");
-        System.out.println("Odd Numbers: "+oddNumList);
-        System.out.println("Even Numbers: "+evenNumList);
-        System.out.println("Prime NUmbers: "+primeNumList);
-
-    }
-
     public static void main(String[] args) {
         List<Integer> numList=Stream.iterate(1, num-> num + 1)
                 .limit(50).collect(Collectors.toList());
 
-        Consumer<List<Integer>> result=NumConsumer::numFilter;
-        result.accept(numList);
+        Consumer<List<Integer>> oddNumList=list->list.stream().filter(num->isOdd(num)).forEach(num->System.out.print(num+" "));
+        Consumer<List<Integer>> evenNumList=list->list.stream().filter(num->isEven(num)).forEach(num->System.out.print(num+" "));
+        Consumer<List<Integer>> primeNumList=list->list.stream().filter(num->isPrime(num)).forEach(num->System.out.print(num+" "));
+
+        System.out.println("------Odd Numbers-----");
+        oddNumList.accept(numList);
+        System.out.println();
+        System.out.println("------Even Numbers-----");
+        evenNumList.accept(numList);
+        System.out.println();
+        System.out.println("------Prime Numbers-----");
+        primeNumList.accept(numList);
     }
 }

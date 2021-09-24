7d3ff766ecefc6ec292d0e3bb44da2aec3a1b565
Dharma Raj Thanait <dharmarajthanait25@gmail.com>
2021-09-24 15:55:18 +0545

    Question no. 5 completed

---
+++ src/main/java/com/assigment3/question5/Sort.java
@@ -0,0 +1,20 @@
+package com.assigment3.question5;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class Sort {
+    public static void main(String[] args) {
+        List<Integer> numList= new Random().ints(100, 1,1000).boxed().collect(Collectors.toList());
+        List<Integer> sortedList=numList.stream().sorted(Comparator.reverseOrder()).collect(Collectors.toList());
+
+        System.out.println("---------Numbers Before Sorting--------");
+        System.out.println(numList);
+        System.out.println("---------Numbers After Sorting--------");
+        System.out.println(sortedList);
+
+    }
+}
